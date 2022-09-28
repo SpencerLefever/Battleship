97193bf9cec6991f96bda3ebfa47bb113dc7f7ba
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-09-28 14:16:47 -0400

    Network debugging

--- ServerTest.java
+++ ServerTest.java
@@ -1,11 +1,23 @@
 // Fig. 24.6: ServerTest.java
 // Test the Server application.
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.swing.JFrame;
 
 public class ServerTest
 {
    public static void main( String args[] )
    {
+      InetAddress ip;
+      try {
+
+         ip = InetAddress.getLocalHost();
+         System.out.println(ip);
+      } catch (UnknownHostException e) {
+         e.printStackTrace();
+      }
       Server application = new Server(); // create server
       application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
       application.runServer(); // run server application
