9f797b4b22e14be21f18854c35dc42db2b976ffb
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-10-07 10:21:49 -0400

    Testing class for just the gui and view class for gui init

---
+++ BattleshipView.java
@@ -0,0 +1,102 @@
+/**
+ * Spencer Lefever 
+ * COSC330 Project 1 Battleship View
+ * 
+ * View class for a player's gui
+ */
+
+ import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import javax.swing.plaf.basic.BasicDesktopIconUI.MouseInputHandler;
+import javax.swing.plaf.basic.BasicTabbedPaneUI.MouseHandler;
+
+ public class BattleshipView extends JFrame{
+
+    final int GRID_LENGTH = 10, GRID_WIDTH = 10;
+    //Declare gui components
+    JFrame mainFrame;
+
+    //Player Grids
+    /*
+    JInternalFrame oceanGrid;
+    JInternalFrame targetGrid;
+    */
+    JButton [] oceanGrid;
+    JButton [] targetGrid;
+
+
+    //Ship Image and dropdown Menu
+    JComboBox shipBox;
+    ImageIcon shipIcon;
+
+    //GUI buttons
+    JButton confirmShips;
+    JButton randomizeShips;
+    JButton restartButton;
+
+    public BattleshipView() {
+      initGUI();
+    }
+
+    private void initGUI() {
+      mainFrame = new JFrame();
+
+      /*
+      oceanGrid = new JInternalFrame();
+      oceanGrid.setTitle("Ocean Grid");
+      oceanGrid.setVisible(true);
+      targetGrid = new JInternalFrame();
+      targetGrid.setTitle("Target Grid");
+      targetGrid.setVisible(true);
+      */
+
+      JButton [] oceanGrid = new JButton[GRID_LENGTH * GRID_WIDTH];
+      JButton [] targetGrid = new JButton[GRID_LENGTH * GRID_WIDTH];
+
+
+      String[] shipList = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
+      shipBox = new JComboBox<String>(shipList);
+      shipBox.setVisible(true);
+      shipBox.addActionListener(shipBox);
+
+      createLayout(oceanGrid, targetGrid, shipBox);
+    }
+
+    private void createLayout(JButton[] oceanGrid, JButton[] targetGrid, JComboBox shipBox ) {
+      Container pane = getContentPane();
+      GroupLayout gl = new GroupLayout(pane);
+      pane.setLayout(gl);
+
+      gl.setAutoCreateContainerGaps(true);
+      gl.setAutoCreateGaps(true);
+
+      gl.setHorizontalGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addGroup(gl.createParallelGroup()
+              .addComponent(oceanGrid)
+              .addGap(30)
+              .addComponent(targetGrid)
+              .addGap(30))
+);
+
+gl.setVerticalGroup(gl.createSequentialGroup()
+      .addGroup(gl.createParallelGroup()
+              .addComponent(oceanGrid)
+              .addComponent(targetGrid)
+      .addGap(30))
+);
+pack();
+   }
+
+    public void actionPerformed(ActionEvent e) {
+      JComboBox cb = (JComboBox)e.getSource();
+      String shipName = (String)cb.getSelectedItem();
+      updateLabel(shipName);
+    }
+
+    public void updateLabel(String shipName) {
+      
+    }
+ }
---
+++ BattleshipViewTest.java
@@ -0,0 +1,14 @@
+/**
+ * Spencer Lefever
+ * COSC330 Project 1
+ * 
+ * Testing class for the battleship
+ * game GUI for one user playing across the network
+ */
+
+ public class BattleshipViewTest{
+    public static void main(String[] args) {
+        BattleshipView test = new BattleshipView();
+        test.setVisible(true);
+    }
+ }
