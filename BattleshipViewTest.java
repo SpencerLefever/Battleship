41ab627684b26c4656675e0e8e5aeb2db8c75966
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-10-23 22:02:04 -0400

    Worked on the gui

---
+++ BattleshipViewTest.java
@@ -0,0 +1,12 @@
+/**
+ * Spencer Lefever
+ * Testing class for battleship view
+ */
+
+ public class BattleshipViewTest {
+    public static void main(String[] args) {
+        BattleshipView view = new BattleshipView();
+
+        view.setVisible(true);
+    }
+ }
