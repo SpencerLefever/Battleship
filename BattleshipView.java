e22a85a5695c0c4ab013bfefecb84983b46d59b5
Spencer Evan Lefever <slefever1@gulls.salisbury.edu>
2022-10-25 15:11:39 -0400

    Controller calls methods to create gui components

--- BattleshipView.java
+++ BattleshipView.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class BattleshipView extends JFrame {
         //private battleshipModel gridModel;
 
         final int GRID_LENGTH = 10;
         final int GRID_WIDTH = 10;
+
+        private BattleshipModel battleshipModel;
         
         private JButton quitButton = new JButton("Quit");
         private JButton restartButton = new JButton("Restart");
@@ -15,6 +18,8 @@ public class BattleshipView extends JFrame {
         private JButton confirmButton = new JButton("Confirm");
         private JPanel oceanGrid = new JPanel();
         private JPanel targetGrid = new JPanel();
+        private JPanel gridPanel;
+        private JPanel buttonPanel;
         private JPanel shipContainer = new JPanel();
         private JPanel shipSelectionPanel = new JPanel();
         private JComboBox<String> shipSelection;
@@ -23,69 +28,99 @@ public class BattleshipView extends JFrame {
         private JRadioButton [][] oceanGridButtonArr = new JRadioButton[GRID_LENGTH][GRID_WIDTH];
         private JRadioButton [][] targetGridButtonArr = new JRadioButton[GRID_LENGTH][GRID_WIDTH];
 
-        public BattleshipView(){
 
+        public BattleshipView(BattleshipModel m){
+
+                battleshipModel = m;
                 //Set size of frame
                 setSize(750,1000);
                 //Set main frame to border layout
                 setLayout(new BorderLayout(50, 50));
 
-                //Initialize JComboBox and values
-                String[] shipList = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
-                shipSelection = new JComboBox<String>(shipList);
-                shipSelection.setVisible(true);
+                initShipSelectionPanel();
 
-                //Initialize ImageIcon to null value
-                shipIcon = new ImageIcon("src/battleship.png");
-                
-                shipLabel = new JLabel(shipIcon, JLabel.CENTER);
-                shipLabel.setVisible(true);
+                initOceanGridButtonArr();
 
-                //Set layout for shipSelectionPanel
-                shipSelectionPanel.setLayout(new GridLayout(2,1,25,25));
+                initTargetGridButtonArr();
 
-                //Add ship combo box and icon to the Ship selection panel
-                shipSelectionPanel.add(shipSelection);
-                shipSelectionPanel.add(shipLabel);
+                initTargetGridPanel();
 
-                //Initialize oceanGridButtonArr
+                initOceanGridPanel();
+
+                initGridPanel();
+
+                initButtonPanel();
+
+                addPanelsToMainFrame();
+            
+
+                // Finalize
+                pack();
+                setResizable(false);
+                setTitle("BattleShip");
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        }
+
+        //Method to initialize JComboBox and ship icons for border layout
+        void initShipSelectionPanel() {
+            //Initialize JComboBox and values
+            String[] shipList = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
+            shipSelection = new JComboBox<String>(shipList);
+            shipSelection.setVisible(true);
+
+            //Initialize ImageIcon to null value
+            shipIcon = new ImageIcon("src/battleship.png");
+            
+            shipLabel = new JLabel(shipIcon, JLabel.CENTER);
+            shipLabel.setVisible(true);
+
+            //Set layout for shipSelectionPanel
+            shipSelectionPanel.setLayout(new GridLayout(2,1,25,25));
+
+            //Add ship combo box and icon to the Ship selection panel
+            shipSelectionPanel.add(shipSelection);
+            shipSelectionPanel.add(shipLabel);
+
+        }
+
+        void initOceanGridButtonArr() {
+            //Initialize oceanGridButtonArr
                 for(int i = 0; i < GRID_LENGTH; i++) {
                         for(int j=0; j< GRID_WIDTH; j++) {
                                 oceanGridButtonArr[i][j] = new JRadioButton();
                         }
-                }
+                }  
+        }
 
-                //Initialize targetGridButtonArr
+        void initTargetGridButtonArr() {
+             //Initialize targetGridButtonArr
                 for(int i = 0; i < GRID_LENGTH; i++) {
                         for(int j=0; j< GRID_WIDTH; j++) {
                                targetGridButtonArr[i][j] = new JRadioButton();
                         }
-                }
-                        
-                // Layout Components
-                JPanel gridPanel = new JPanel();
-                JPanel buttonPanel = new JPanel();
-                
-                
-                //Add components to button panel
-                buttonPanel.setLayout(new GridLayout(3,1,25,25));
-                buttonPanel.add(quitButton);
-                buttonPanel.add(restartButton);
-                buttonPanel.add(confirmButton);
-                
-                
-                //Add components to grid panel
-                gridPanel.setLayout(new GridLayout(2, 1, 50, 50));
-                
+                }           
+        }
 
-                //Variable to hold the value for space between buttons in grid
-                final int buttonSpacer = 5;
+        void initGridPanel() {
+            // Layout Components
+            gridPanel = new JPanel();
 
-                //Set ocean and target grid panels to grid layout
-                oceanGrid.setLayout(new GridLayout(GRID_LENGTH, GRID_WIDTH, buttonSpacer, buttonSpacer));
-                targetGrid.setLayout(new GridLayout(GRID_LENGTH, GRID_WIDTH, buttonSpacer, buttonSpacer));
+            //Add components to grid panel
+            gridPanel.setLayout(new GridLayout(2, 1, 50, 50));
+                
+            //Add grid to the grid panel
+            gridPanel.add(oceanGrid);
+            gridPanel.add(targetGrid);                
+            
+            gridPanel.setVisible(true);
+        }
 
-                //Add buttons to ocean grid panel
+        void initOceanGridPanel() {
+            final int buttonSpacer = 5;
+            //Set ocean and target grid panels to grid layout
+            oceanGrid.setLayout(new GridLayout(GRID_LENGTH, GRID_WIDTH, buttonSpacer, buttonSpacer));
+             //Add buttons to ocean grid panel
                 for(int i = 0; i < GRID_LENGTH; i++) {
                         for(int j=0; j< GRID_WIDTH; j++) {
                                 oceanGrid.add(oceanGridButtonArr[i][j]);
@@ -93,8 +128,15 @@ public class BattleshipView extends JFrame {
                         }
                 }
                 oceanGrid.setVisible(true);
+        }
 
-                //Add buttons to target grid panel
+        void initTargetGridPanel() {
+            final int buttonSpacer = 5;
+
+            targetGrid.setLayout(new GridLayout(GRID_LENGTH, GRID_WIDTH, buttonSpacer, buttonSpacer));
+
+
+             //Add buttons to target grid panel
                 for(int i = 0; i < GRID_LENGTH; i++) {
                         for(int j=0; j< GRID_WIDTH; j++) {
                                targetGrid.add(targetGridButtonArr[i][j]);
@@ -102,38 +144,28 @@ public class BattleshipView extends JFrame {
                         }
                 }
                 targetGrid.setVisible(true);
+        }
 
-                //Add grid to the grid panel
-                gridPanel.add(oceanGrid);
-                gridPanel.add(targetGrid);                
+        void initButtonPanel() {
+            buttonPanel = new JPanel();
                 
-                gridPanel.setVisible(true);
-        
+                
+            //Add components to button panel
+            buttonPanel.setLayout(new GridLayout(1,3,25,25));
+            buttonPanel.add(fireButton);
+            buttonPanel.add(confirmButton);
+            buttonPanel.add(restartButton);
+        }
 
-                
-                
-                
+        void addPanelsToMainFrame() {
                 //Add panels to main frame
-                add(buttonPanel, BorderLayout.WEST);
+                add(buttonPanel, BorderLayout.PAGE_END);
                 add(gridPanel, BorderLayout.CENTER);
                 add(shipSelectionPanel, BorderLayout.EAST);
                 
-        
-
-                // Finalize
-                pack();
-                setResizable(true);
-                setTitle("BattleShip");
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         }
-        
-        /*
+
         // Add listeners
-        void addQuitListener(ActionListener q){
-                quitButton.addActionListener(q);
-        }
-
         void addRestartListener(ActionListener r){
                 restartButton.addActionListener(r);
         }
@@ -146,9 +178,6 @@ public class BattleshipView extends JFrame {
                 confirmButton.addActionListener(c);
         }
 
-        void addRandomListener(ActionListener r){
-                randomButton.addActionListener(r);
-        }
-        */
+        
 
 }
